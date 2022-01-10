def63a2a0fadb23ddb146ce8858999939459d709
zariririririri <sara.2267@icloud.com>
2022-01-11 00:18:01 +0900

    商品削除機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -39,9 +39,9 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    # product = Product.find(params[:id])
-    # product.destroy
-    # redirect_to root_path
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to root_path
   end
 
 private
