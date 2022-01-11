897b81670171c4f43bd107548feff65da52b7ea5
zariririririri <sara.2267@icloud.com>
2022-01-11 12:25:55 +0900

    修正　＃1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:edit, :show]
+  before_action :set_product,        only: [:edit, :show]
+  before_action :redirect_product,   only: [:edit, :destroy]
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -20,15 +21,11 @@ class ProductsController < ApplicationController
    end
  
   def show
-    @product = Product.find(params[:id])
   end
-
+  
   def edit
-    unless current_user.id == @product.user_id
-      redirect_to root_path 
-    end
   end 
-
+  
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
@@ -53,5 +50,12 @@ private
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def redirect_product
+    @product = Product.find(params[:id])
+    unless current_user.id == @product.user_id
+      redirect_to root_path 
+    end
   end
  end
