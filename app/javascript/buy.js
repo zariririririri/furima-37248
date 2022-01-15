014990c57219895645b8f51e765395cb1fdb4f57
zariririririri <sara.2267@icloud.com>
2022-01-15 23:09:58 +0900

    commit #1

--- app/javascript/buy.js
+++ app/javascript/buy.js
@@ -13,17 +13,16 @@ Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
       exp_month: formData.get("buy_receiver[exp_month]"),
       exp_year: `20${formData.get("buy_receiver[exp_year]")}`,
     };
-    console.log(card)
-    Payjp.createToken(card, (status, response) => {
-      console.log(response);
-      if (status == 200) {
+ 
+Payjp.createToken(card, (status, response) => {
+   if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-
-    document.getElementById("card-number").removeAttribute("name");
+      
+document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
       document.getElementById("card-exp-month").removeAttribute("name");
       document.getElementById("card-exp-year").removeAttribute("name");
@@ -33,4 +32,4 @@ Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   });
 };
     
-window.addEventListener("load", pay);
+    window.addEventListener("load", pay);
--- spec/models/buy_receiver_spec.rb
+++ spec/models/buy_receiver_spec.rb
@@ -51,6 +51,18 @@ context '内容に問題がある場合' do
     expect(@buy_receiver.errors.full_messages).to include("Post code can't be blank")
   end
 
+  it '市区町村が空だと保存できないこと' do
+    @buy_receiver.city = nil
+    @buy_receiver.valid?
+    expect(@buy_receiver.errors.full_messages).to include("City can't be blank")
+  end
+
+  it '番地が空だと保存できないこと' do
+    @buy_receiver.house_number = nil
+    @buy_receiver.valid?
+    expect(@buy_receiver.errors.full_messages).to include("House number can't be blank")
+  end
+
   it 'post_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
     @buy_receiver.post_code = '1234567'
     @buy_receiver.valid?
@@ -58,7 +70,13 @@ context '内容に問題がある場合' do
   end
 
   it 'prefectureを選択していないと保存できないこと' do
-    @buy_receiver.prefecture_id = ''
+    @buy_receiver.prefecture_id = nil
+    @buy_receiver.valid?
+    expect(@buy_receiver.errors.full_messages).to include("Prefecture can't be blank")
+  end
+
+  it 'prefecture-idの1を選択された場合保存できないこと' do
+    @buy_receiver.prefecture_id = '1'
     @buy_receiver.valid?
     expect(@buy_receiver.errors.full_messages).to include("Prefecture can't be blank")
   end
@@ -69,8 +87,14 @@ context '内容に問題がある場合' do
     expect(@buy_receiver.errors.full_messages).to include('Telephone number is invalid')
   end
 
-  it 'telephone_numberは、10桁以上11桁以内の半角数値のみ保存可能なこと' do
-    @buy_receiver.telephone_number = '1234567890123' 
+  it 'telephone_numberが空だと保存できない' do
+    @buy_receiver.telephone_number = nil 
+    @buy_receiver.valid?
+    expect(@buy_receiver.errors.full_messages).to include("Telephone number is invalid")
+  end
+
+  it 'telephone_numberは数値のみでしか保存できない' do
+    @buy_receiver.telephone_number = '壱参伍'
     @buy_receiver.valid?
     expect(@buy_receiver.errors.full_messages).to include("Telephone number is invalid")
   end
